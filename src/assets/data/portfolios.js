f7874baf5fdb82a66471790fdc5d2b2aa2ef423e
Diki Armanda <diki.armada5@gmail.com>
2024-08-28 17:14:13 +0700

    feat: update portfolio

---
+++ src/assets/data/portfolios.js
@@ -0,0 +1,24 @@
+import POSTER from '../images/portfolios/index'
+
+const PORTFOLIOS = [
+  {
+    title: "Cari Jurnal",
+    desc: "Aplikasi ini dibuat untuk memudahkan pengguna untuk mencari portal jurnal yang sesuai dengan preferensi pengguna. Aplikasi ini dilengkapi dengan informasi publication fee yang dapat membantu pengguna mengetahui biaya yang dibutuhkan untuk mempublikasi jurnal di portal jurnal tersebut.",
+    img: POSTER.cariJurnal,
+    link: "https://cari-jurnal.vercel.app/"
+  },
+  {
+    title: "VARK Model",
+    desc: "Aplikasi VARK Model adalah alat edukasi yang membantu pengguna mengidentifikasi dan memahami preferensi gaya belajar mereka—Visual, Aural, Read/Write, atau Kinesthetic—melalui kuis analisis. Berdasarkan hasil, aplikasi ini memberikan saran dan strategi belajar yang disesuaikan, sehingga pengguna dapat belajar lebih efektif sesuai dengan gaya mereka. Cocok untuk pelajar, pendidik, dan siapa saja yang ingin meningkatkan metode pembelajaran pribadi.",
+    img: POSTER.varkModel,
+    link: "https://dikiarmanda.github.io/vark-model/"
+  },
+  {
+    title: "Aplikasi Sistem Persamaan Non Linier",
+    desc: "Aplikasi Sistem Persamaan Non Linier merupakan tugas dari mata kuliah metode numberik.",
+    img: POSTER.aplikasiSistemPersamaanNonLinier,
+    link: "https://diki001metnum.netlify.app/"
+  },
+]
+
+export default PORTFOLIOS
