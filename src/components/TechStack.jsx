e610e32474bede2b17f71c394b62f6bf263cdfb3
Diki Armanda <diki.armada5@gmail.com>
2024-08-29 08:29:00 +0700

    feat: update tech stack

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -7,10 +7,13 @@ const TechStack = () => {
         <pre data-prefix="$"><code>Installing...</code></pre>
       </div>
       <div className="flex w-full gap-3 md:gap-10 justify-center mt-10 mx-5 px-20">
-        <img className="h-[75px]" src="https://cdn.brandfetch.io/ide68-31CH/theme/dark/logo.svg?k=id64Mup7ac&t=1724248340308?t=1724248340308" alt="" />
-        <img className="h-[75px]" src="https://cdn.brandfetch.io/idZyDqb1HB/theme/dark/logo.svg?k=id64Mup7ac&t=1716372510436?t=1716372510436" alt="" />
-        <img className="h-[75px]" src="https://cdn.brandfetch.io/id0AgeLF7b/w/400/h/400/theme/dark/icon.png?k=id64Mup7ac&t=1718751632416?t=1718751632416" alt="" />
-        <img className="h-[75px]" src="https://cdn.brandfetch.io/idq5ck33b2/w/300/h/300/theme/light/logo.png?k=id64Mup7ac&t=1724216622107?t=1724216622107" alt="" />
+        <img className="h-[75px]" src="https://cdn.brandfetch.io/ide68-31CH/theme/dark/logo.svg?k=id64Mup7ac&t=1724248340308?t=1724248340308" alt="laravel" />
+        <img className="h-[75px]" src="https://cdn.brandfetch.io/idZyDqb1HB/theme/dark/logo.svg?k=id64Mup7ac&t=1716372510436?t=1716372510436" alt="codeigniter" />
+        <img className="h-[75px]" src="https://cdn.brandfetch.io/id0AgeLF7b/w/400/h/400/theme/dark/icon.png?k=id64Mup7ac&t=1718751632416?t=1718751632416" alt="reactjs" />
+        <img className="h-[75px]" src="https://cdn.brandfetch.io/idq5ck33b2/w/300/h/300/theme/light/logo.png?k=id64Mup7ac&t=1724216622107?t=1724216622107" alt="js" />
+        <img className="h-[75px]" src="https://cdn.brandfetch.io/idBdG8DdKe/theme/dark/logo.svg?k=id64Mup7ac&t=1667573657581?t=1667573657581" alt="mysql" />
+        <img className="h-[75px]" src="https://cdn.brandfetch.io/id4Ha8BARu/theme/dark/symbol.svg?k=id64Mup7ac&t=1671618196174?t=1671618196174" alt="bootstrap" />
+        <img className="h-[75px]" src="https://cdn.brandfetch.io/idMNEQh7-0/theme/dark/symbol.svg?k=id64Mup7ac&t=1687779047045?t=1687779047045" alt="tailwindcss" />
       </div>
     </div>
   )
