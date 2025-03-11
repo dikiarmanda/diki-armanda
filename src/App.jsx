e0f0d7c190cfbcab352b534d0711583a874e6e12
dikiarmanda <diki.armada5@gmail.com>
2025-03-11 11:44:08 +0700

    fix: container

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className="container">
+    <div className="container mx-auto">
       <Navbar />
       <main>
         <Hero />
